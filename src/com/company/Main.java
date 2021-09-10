ab25dc395adeb3bacd836b20812bd78733a81c9e
Legend0131 <nip2001@mail.ru>
2021-09-10 15:01:56 +0600

    Lesson_6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class Main {
 
+    //Решение методом Крамера
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         double a1;
